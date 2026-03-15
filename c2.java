4d8843c093e56de855e5f4c986b3c97c28c1466f
p4-49m <sengxiangt2@gmail.com>
2026-03-16 00:28:43 +0800

    passing parameter

--- c2.java
+++ c2.java
@@ -1,5 +1,14 @@
 public class c2{
+    
+    public static void nPrintln(String message, int n) {  
+    for (int i = 0; i < n; i++)
+        System.out.println(message);
+    
+    
+    } 
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!");
+        nPrintln("Welcome to Java", 5); 
+        nPrintln("Computer Science", 15);
     }
 }
