b9b602d113a65737c5b18a9a3959ccc34180634a
p4-49m <sengxiangt2@gmail.com>
2026-03-15 22:40:04 +0800

    SCANNNER

--- est.java
+++ est.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class est {
-    String test = JOptionPane.showInputDialog(null, "Hello World!!!!'" , "Hell nah", JOptionPane.QUESTION_MESSAGE);
-    
+    // String test = JOptionPane.showInputDialog(null, "Hello World!!!!'" , "Hell nah", JOptionPane.QUESTION_MESSAGE);
+    System.out.print("Enter a double value: ");
+    Scanner scanner = new Scanner(System.in);
+    double d = scanner.nextDouble();
+
 }
