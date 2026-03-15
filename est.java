caa7a54dc46faa3bfdf0c927ef1761676b58649e
p4-49m <sengxiangt2@gmail.com>
2026-03-15 23:17:15 +0800

    unicode

--- est.java
+++ est.java
@@ -3,10 +3,15 @@ import java.util.Scanner;
 public class est {
     public static void main(String[] args) {
     // String test = JOptionPane.showInputDialog(null, "Hello World!!!!'" , "Hell nah", JOptionPane.QUESTION_MESSAGE);
-    System.out.print("Enter a double value: ");
-    Scanner scanner = new Scanner(System.in);
-    double d = scanner.nextDouble();
-    System.out.println("You entered: " + d);
+    // System.out.print("Enter a double value: ");
+    // Scanner scanner = new Scanner(System.in);
+    // double d = scanner.nextDouble();
+    // System.out.println("You entered: " + d);
+
+    char letter = '\u0041';
+    char numChar = '\u0034';
+    System.out.println("The letter is: " + letter);
+    System.out.println("The number character is: " + numChar);
     }
 
 }
