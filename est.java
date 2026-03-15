c39cdbb253ffd24fc17349f1913bf288feaec9a6
p4-49m <sengxiangt2@gmail.com>
2026-03-15 23:30:37 +0800

    c2

--- est.java
+++ est.java
@@ -21,9 +21,9 @@ public class est {
     // String s = "Chapter" + 2;
     // System.out.println(s);
 
-    int quantity = 10;
-    double salesAmt = 250.80;
-    System.out.printf("Sales-> Qty: %d, Amt: RM%.2f\n", quantity, salesAmt);
+    // int quantity = 10;
+    // double salesAmt = 250.80;
+    // System.out.printf("Sales-> Qty: %d, Amt: RM%.2f\n", quantity, salesAmt);
     //%s = string, %d = int, %f = floating, %c = char
 
 
