ef6f0a91ee4a6d0acff77ee47ce03782ca962bc2
p4-49m <sengxiangt2@gmail.com>
2026-03-15 22:42:01 +0800

    psvm

--- est.java
+++ est.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
+
 public class est {
+    public static void main(String[] args) {
     // String test = JOptionPane.showInputDialog(null, "Hello World!!!!'" , "Hell nah", JOptionPane.QUESTION_MESSAGE);
     System.out.print("Enter a double value: ");
     Scanner scanner = new Scanner(System.in);
