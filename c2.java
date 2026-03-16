9602653feab65f01d1a773d6373d47b825714da3
p4-49m <sengxiangt2@gmail.com>
2026-03-16 13:44:54 +0800

    pass by value

--- c2.java
+++ c2.java
@@ -1,14 +1,27 @@
 public class c2{
     
-    public static void nPrintln(String message, int n) {  
-    for (int i = 0; i < n; i++)
-        System.out.println(message);
+    // public static void nPrintln(String message, int n) {  
+    // for (int i = 0; i < n; i++)
+    //     System.out.println(message);
     
     
-    } 
+    // } 
     
-    public static void main(String[] args) {
-        nPrintln("Welcome to Java", 5); 
-        nPrintln("Computer Science", 15);
-    }
+    // public static void main(String[] args) {
+    //     nPrintln("Welcome to Java", 5); 
+    //     nPrintln("Computer Science", 15);
+    // }
+
+    // public static void main(String[] args) {
+    //     int num = 1;
+    //     change(num);
+    // }
+
+    // public static void change(int num) {
+    //     System.out.println("Before  : " + num); // 1
+    //     num ++; // 1+1
+    //     System.out.println("After : " + num); // 2
+    // }
+
+
 }
