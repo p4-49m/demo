9e69e981e01149d45c0a18d20539e674196b1484
p4-49m <sengxiangt2@gmail.com>
2026-03-16 18:17:32 +0800

    Ambiguous Invocation

--- c2.java
+++ c2.java
@@ -24,4 +24,43 @@ public class c2{
     // }
 
 
+    //     public static void main(String[] args) {
+    //         int i = 5,  j = 2;
+    //         int k = max(i, j);
+    //         System.out.println("The maximum between " + i +  " and " + j + " is " + k);
+    // }
+
+    // /* Return the max between two numbers */
+    // public static int max(int num1, int num2) {
+    //     int result;
+
+    //     if (num1 > num2)
+    //     result = num1;
+    //     else
+    //     result = num2;
+
+    //     return result;
+    // }
+
+    
+
+    public static double max1(int num1, double num2) { 
+        if (num1 > num2)
+            return num1;
+        else
+            return num2;
+    }
+    public static double max2(double num1, int num2) {
+        if (num1 > num2)
+            return num1;
+        else
+            return num2;     
+  }
+  static double x = max1(1, 2);
+  public static void main(String[] args) {
+    System.out.println(x);
+  }
+
+
 }
+
